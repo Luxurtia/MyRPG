b21be3c8372d36cdbfd0985a63831a7c52d9794f
Luxurtia <dgd17581@gmail.com>
2026-06-01 16:27:09 +0900

    PLAYER 기본 조작 및 스텟 설정
    ㅇㅇ

--- src/main/java/com/gema/entity/Entity.java
+++ src/main/java/com/gema/entity/Entity.java
@@ -1,4 +1,54 @@
 package com.gema.entity;
 
-public class Entity {
+import java.awt.Graphics2D;
+/*
+    interface
+        규칙 정의
+        여러개 상속 가능
+        변수 사실상 없음
+        implements
+
+    abstract
+        공통기능으로 묶기
+        1개만 가능
+        변수 가능
+        extend
+        자식 클래스가 직접 구현 해야됨
+        ex) 플레이어, 몹 npc => 체력 위치 이동속도 등 공통 데이터 존재
+*/
+public abstract class Entity {      // 게임 내 존재 하는 객체
+    public int worldX, worldY;      // 월드 좌표 (픽셀)
+    public int speed;               // 이동속도
+
+    public int MaxHP;               // 최대 체력
+    public int hp;                  // 현재 체력
+    public int MaxMP;               // 최대 마나
+    public int mp;                  // 현재 마나
+    public int att;                 // 공격력
+    public int Matt;                // 마공
+    public int def;                 // 방어력
+    public int Mdef;                // 마방
+
+    public enum Direction {     // 방향 열거
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        NONE
+    }
+
+    public Direction direction = Direction.NONE;        // 현재 방향
+
+    public abstract void update();      // 매 프레임 로직 업뎃
+
+    public abstract void render(Graphics2D g2, int ScreenX, int ScreenY);       // 매 프레임 렌더링
+
+    public void setTilePosition(int col, int row) {     // 타일 좌표를 픽셀 좌표로 변환
+        worldX = col * 48;
+        worldY = row * 48;
+    }
+
+    public boolean isAlive() {      // 체력이 0 보다 크면 살아 있음
+        return hp > 0;
+    }
 }
--- src/main/java/com/gema/entity/Player.java
+++ src/main/java/com/gema/entity/Player.java
@@ -1,4 +1,62 @@
 package com.gema.entity;
 
-public class Player {
+import com.gema.system.Action;
+import com.gema.system.InputHandler;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
+public class Player extends Entity{
+    private final InputHandler input;       // 키 입력 받아서 이동에 사용
+
+    public Player(InputHandler input) {
+        this.input = input;
+
+        // 기본 스탯 설정
+        speed   = 4;
+        MaxHP   = 200;
+        hp      = 100;
+        MaxMP   = 200;
+        mp      = 100;
+        att     = 10;
+        Matt    = 15;
+        def     = 5;
+        Mdef    = 15;
+    }
+
+    @Override
+    public void update() {
+        handleMovement();
+    }
+
+    @Override
+    public void render(Graphics2D g2, int screenX, int screenY) {
+        // 임시 placeholer
+
+        g2.setColor(Color.BLUE);
+        g2.fillRect(screenX, screenY, 48, 48);
+    }
+
+    private void handleMovement() {
+        if(input.isHeld(Action.MOVE_UP)) {
+            worldY -= speed;
+            direction = Direction.UP;
+        }
+
+        if(input.isHeld(Action.MOVE_DOWN)) {
+            worldY += speed;
+            direction = Direction.DOWN;
+        }
+
+        if(input.isHeld(Action.MOVE_LEFT)) {
+            worldY -= speed;
+            direction = Direction.LEFT;
+        }
+
+        if(input.isHeld(Action.MOVE_RIGHT)) {
+            worldY += speed;
+            direction = Direction.RIGHT;
+        }
+    }
 }
